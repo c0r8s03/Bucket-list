1d16008f23e241a5bdc5a930af729f18b8e23455
c0r8s03 <c0r8s03@gmail.com>
2017-08-04 20:12:00 +0000

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px'});
+
+  
+  $(function(){
+    
+    var myimg1 = "<img src='images/Nerpa.jpg'>";
+    var myimg = "<img src='images/Omul.jpg'>";
+    
+      $('.icon').popover({content:myimg, html:'true', placement:'left'}); 
+      $('.icon1').popover({
+          content:myimg1,
+          html:'true',
+          placement:'left'}); 
+    
+  });
+  
